ac072bad04c5ece332728244daea404cddd90af6
max <max.smith@duke.edu>
2020-04-17 12:21:16 -0400

    tested assembler output with file in main

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,30 @@ import utility.io.Input;
 import utility.io.Output;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main (String[] args) throws ParserConfigurationException, SAXException, IOException {
-        String ISAfile = "src/data/MIPS/ece350ISA.xml";
-        XMLReader reader = new XMLReader(ISAfile);
-        ISA myISA = reader.getISA();
-        Assembler myAssembler = new Assembler(myISA);
-        List<String> text = new ArrayList<>(List.of(
-                "add $t9, $t2, $t1 #Hello world",
-                "sub $a0, $a1, $a2 #Testing comments",
-                "#This is a comment",
-                "\n #Please delete me",
-                "bne $ra, $gp, $fp #Please delete me too"));
-        Input input = new Input(text);
-        Output output = myAssembler.assemble(input);
-        String path = "data/test/srcmain";
-        output.write(FileType.LOGISM, path);
+    public static void main (String[] args)  {
+        try {
+            String ISAfile = "src/data/MIPS/ece350ISA.xml";
+            XMLReader reader = new XMLReader(ISAfile);
+            ISA myISA = reader.getISA();
+            Assembler myAssembler = new Assembler(myISA);
+            String filePath = "data/test/fullALUtest.s";
+            File text = new File(filePath);
+            Input input = new Input(text);
+            Output output = myAssembler.assemble(input);
+            String path = "data/test/srcmain";
+            output.write(FileType.LOGISM, path);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.out.println("Could not assemble file");
+            e.printStackTrace();
+        }
+
     }
 
 }
