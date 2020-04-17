97a1efd58fd6d01e52efd88ff0c1e83bce6d8017
max <max.smith@duke.edu>
2020-04-17 12:24:12 -0400

    added segmentation to main method to prime user input from gui

--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,21 @@ public class Main {
 
     public static void main (String[] args)  {
         try {
+            // CODE WHICH WILL BE USER INPUT PARAMETERS
             String ISAfile = "src/data/MIPS/ece350ISA.xml";
+            String inputPath = "data/test/fullALUtest.s";
+            String outputPath = "data/test/srcmain";
+            File text = new File(inputPath);
+
+            // MODEL CODE TO GET TO OUTPUT
             XMLReader reader = new XMLReader(ISAfile);
             ISA myISA = reader.getISA();
             Assembler myAssembler = new Assembler(myISA);
-            String filePath = "data/test/fullALUtest.s";
-            File text = new File(filePath);
             Input input = new Input(text);
+
+            // FINAL OUTPUT CONSTRUCTION AND WRITE
             Output output = myAssembler.assemble(input);
-            String path = "data/test/srcmain";
-            output.write(FileType.LOGISM, path);
+            output.write(FileType.LOGISM, outputPath);
         } catch (ParserConfigurationException | SAXException | IOException e) {
             System.out.println("Could not assemble file");
             e.printStackTrace();
